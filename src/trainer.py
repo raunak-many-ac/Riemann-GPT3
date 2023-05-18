fe9bbc32bdd9951eca48b9e19054952c7ce8726f
raunak-many-ac <raunak.smvit@gmail.com>
2023-05-18 19:00:14 +0530

    inferrence_test_30000

--- src/trainer.py
+++ src/trainer.py
@@ -8,36 +8,60 @@ import generateDataset as GenerateDataset
 from constants import Constants, ExtractionConstants
 import utils as utils
 
-CREATION_COUNT = 4000
-noOfZeroesOnCriticalAxisToGenerate = 500
+CREATION_COUNT = 6500
+noOfZeroesOnCriticalAxisToGenerate = 750
 
-# load the previously generated data points in a map
+## load the previously generated data points in a map
 previouslyGeneratedDataset: list[dict] = utils.getInputJsonListFromFile(
     Constants.PreviousDatasets.pathTo_22000_dataPoints
 )
 allPreviouslyGeneratedOutputs: set[mpmath.mpc] = {
-    utils.stringToComplexNumber(previouslyGeneratedData[ExtractionConstants.completion]) if previouslyGeneratedData[ExtractionConstants.completion] != Constants.noSolution else None
+    utils.stringToComplexNumber(previouslyGeneratedData[ExtractionConstants.completion])
+    if previouslyGeneratedData[ExtractionConstants.completion] != Constants.noSolution
+    else None
     for previouslyGeneratedData in previouslyGeneratedDataset
 }
 allPreviouslyGeneratedOutputs.remove(None)
 
-dictionaryOfZetaZeroes: dict = GenerateDataset.generateTheCriticalLineZeroes(
-    noOfZeroesOnCriticalAxisToGenerate
-)
+# generate zeroes in critical axis
+dictionaryOfZetaZeroes: dict = {}
+
+while len(dictionaryOfZetaZeroes) < noOfZeroesOnCriticalAxisToGenerate:
+    temp: dict = GenerateDataset.generateTheCriticalLineZeroes(
+        noOfZeroesOnCriticalAxisToGenerate - len(dictionaryOfZetaZeroes)
+    )
+    dictionaryOfZetaZeroes.update(
+        utils.removeKeysFromMap1(temp, allPreviouslyGeneratedOutputs)
+    )
+
 utils.convertToFeedablePromptAndPutInJsonFile(
     dictionaryOfZetaZeroes, Constants.pathToCriticalZeroes
 )
 
-dictionaryOfZetaZeroes = GenerateDataset.generateInfeasibleZetaZeroes(
-    noOfZeroesOnCriticalAxisToGenerate
-)
+# generate infeasible zeroes
+dictionaryOfInfeasibleZeroes: dict = {}
+
+while len(dictionaryOfInfeasibleZeroes) < noOfZeroesOnCriticalAxisToGenerate:
+    temp = GenerateDataset.generateInfeasibleZetaZeroes(
+        noOfZeroesOnCriticalAxisToGenerate - len(dictionaryOfInfeasibleZeroes)
+    )
+    dictionaryOfInfeasibleZeroes.update(
+        utils.removeKeysFromMap1(temp, allPreviouslyGeneratedOutputs)
+    )
+
 utils.convertToFeedablePromptAndPutInJsonFile(
-    dictionaryOfZetaZeroes, Constants.pathToInfeasibleZetaZeroes
+    dictionaryOfInfeasibleZeroes, Constants.pathToInfeasibleZetaZeroes
 )
 
-dictionaryOfZetaZeroes = GenerateDataset.generateRandomZetaFunctionValuesWithRanges(
-    CREATION_COUNT
-)
+# generate random feasible points other than critical axis
+dictionaryOfZetaZeroes = {}
+
+while len(dictionaryOfZetaZeroes) < CREATION_COUNT:
+    temp = GenerateDataset.generateRandomZetaFunctionValuesWithRanges(CREATION_COUNT - len(dictionaryOfZetaZeroes))
+    dictionaryOfZetaZeroes.update(
+        utils.removeKeysFromMap1(temp, allPreviouslyGeneratedOutputs)
+    )
+
 utils.convertToFeedablePromptAndPutInJsonFile(
     dictionaryOfZetaZeroes, Constants.pathToGeneralValues
 )
--- src/utils.py
+++ src/utils.py
@@ -10,19 +10,33 @@ def putInJsonRaw(data: dict, filePath: str):
     jsonData = json.dumps(data, indent=4)
     output_file.write(jsonData)
 
+
 # this function takes inference results and serialises to be dumped in a file
-def serialiseInferenceResultsAndPutInFile(data: dict[tuple[mpmath.mpc, mpmath.mpf, mpmath.mpf], dict[str, mpmath.mpc]], filePath: str):
-    serialisedDict: dict[str,dict[str, str]] = {}
+def serialiseInferenceResultsAndPutInFile(
+    data: dict[tuple[mpmath.mpc, mpmath.mpf, mpmath.mpf], dict[str, mpmath.mpc]],
+    filePath: str,
+):
+    serialisedDict: dict[str, dict[str, str]] = {}
     for key in data:
         inputToEta = f"({complexNumberToString(key[0])}, {complexNumberToString(key[1])}, {complexNumberToString(key[2])})"
-        expectedInputToZeta = complexNumberToString(data[key][ExtractionConstants.expectedValue])
-        inferredInputToZeta = complexNumberToString(data[key][ExtractionConstants.inferredValue])
-        serialisedDict[inputToEta] = {ExtractionConstants.expectedValue: expectedInputToZeta, ExtractionConstants.inferredValue: inferredInputToZeta}
+        expectedInputToZeta = complexNumberToString(
+            data[key][ExtractionConstants.expectedValue]
+        )
+        inferredInputToZeta = complexNumberToString(
+            data[key][ExtractionConstants.inferredValue]
+        )
+        serialisedDict[inputToEta] = {
+            ExtractionConstants.expectedValue: expectedInputToZeta,
+            ExtractionConstants.inferredValue: inferredInputToZeta,
+        }
 
     putInJsonRaw(serialisedDict, filePath)
 
-def convertToSerialisableJsonAndPutInJsonFile(data: dict[mpmath.mpc, tuple[mpmath.mpc, mpmath.mpf, mpmath.mpf]], filePath: str):
-    serialisedDict: dict[str,str] = {}
+
+def convertToSerialisableJsonAndPutInJsonFile(
+    data: dict[mpmath.mpc, tuple[mpmath.mpc, mpmath.mpf, mpmath.mpf]], filePath: str
+):
+    serialisedDict: dict[str, str] = {}
     for key in data:
         inputToZeta = complexNumberToString(key)
         inputToEta = f"({complexNumberToString(data[key][0])}, {complexNumberToString(data[key][1])}, {complexNumberToString(data[key][2])})"
@@ -30,40 +44,48 @@ def convertToSerialisableJsonAndPutInJsonFile(data: dict[mpmath.mpc, tuple[mpmat
 
     putInJsonRaw(serialisedDict, filePath)
 
+
 # convert a dictionary with key as prompt data (zetaValue, L, R) and value as inputValue to json
 # but it should be in the specific format as per the GPT-3 doc: https://beta.openai.com/docs/guides/fine-tuning/prepare-training-data
-def convertToFeedablePromptAndPutInJsonFile(data: dict[tuple[mpmath.mpc, mpmath.mpf, mpmath.mpf], mpmath.mpc], filePath: str = "./dataset.json"):
+def convertToFeedablePromptAndPutInJsonFile(
+    data: dict[tuple[mpmath.mpc, mpmath.mpf, mpmath.mpf], mpmath.mpc],
+    filePath: str = "./dataset.json",
+):
     data = convertToFeedablePrompt(data)
     putInJsonRaw(data, filePath)
 
+
 def keepMaxFiveDigitsAfterDecimal(decimalStr):
-    fiveDigitsAfterDecimal = min(len(decimalStr),decimalStr.find(".") + 6)
+    fiveDigitsAfterDecimal = min(len(decimalStr), decimalStr.find(".") + 6)
     decimalStr = decimalStr[:fiveDigitsAfterDecimal]
     return decimalStr
 
-# will convert complex number to string input i.e. 
+
+# will convert complex number to string input i.e.
 # mpmath.mpc(1.10211, +0.02171) --> str("1.10211 + i0.02171")
 def complexNumberToString(complexNumber: mpmath.mpc) -> str:
     if isinstance(complexNumber, str):
         return complexNumber
 
     realPart = complexNumber.real
-    realPartAsString = f'{realPart}'
+    realPartAsString = f"{realPart}"
     realPartAsString = keepMaxFiveDigitsAfterDecimal(realPartAsString)
 
     imagPart = complexNumber.imag
-    imagPartAsString = f'{abs(imagPart)}'
+    imagPartAsString = f"{abs(imagPart)}"
     imagPartAsString = keepMaxFiveDigitsAfterDecimal(imagPartAsString)
 
     return f"{realPartAsString} {'+' if imagPart >= 0 else '-'} i{imagPartAsString}"
 
-# will convert string input to complex i.e. 
+
+# will convert string input to complex i.e.
 # str("1.10211 + i0.02171") --> mpmath.mpc(1.10211, +0.02171)
 def stringToComplexNumber(stringNumber: str) -> mpmath.mpc:
     splittedNumbers = stringNumber.split(" ")
     realPart = splittedNumbers[0]
     imaginaryPart = splittedNumbers[1] + splittedNumbers[2][1:]
     return mpmath.mpc(realPart, imaginaryPart)
+
 
 def promptCompletionFormat(key, value):
     output = complexNumberToString(value)
@@ -72,9 +94,13 @@ def promptCompletionFormat(key, value):
     low = keepMaxFiveDigitsAfterDecimal(f"{key[1]}")
     high = keepMaxFiveDigitsAfterDecimal(f"{key[2]}")
 
-    promptDict: dict[str, str] = {"prompt": f"Zetavalue:{zetaValue},low:{low},high:{high}", "completion":output}
-    
+    promptDict: dict[str, str] = {
+        "prompt": f"Zetavalue:{zetaValue},low:{low},high:{high}",
+        "completion": output,
+    }
+
     return promptDict
+
 
 def convertToFeedablePrompt(data: dict):
     feedablePromptDict = []
@@ -83,10 +109,12 @@ def convertToFeedablePrompt(data: dict):
         feedablePromptDict.append(promptDict)
     return feedablePromptDict
 
+
 def getInputJsonListFromFile(filePath: str) -> list[dict]:
     jsonFile = open(filePath)
     jsonList: list = json.load(jsonFile)
     return jsonList
+
 
 def mergeTwoJsonsAndShuffle(jsonPaths: list, mergedJsonPath: str = "./dataset.json"):
     mergedJsons: list = []
@@ -100,7 +128,15 @@ def mergeTwoJsonsAndShuffle(jsonPaths: list, mergedJsonPath: str = "./dataset.js
     jsonData = json.dumps(mergedJsons, indent=4)
     output_file.write(jsonData)
 
+
+# if dict1 contains any key mentioned in keysToRemove then that entry will be removed
+def removeKeysFromMap1(dict1: dict, keysToRemove: set[mpmath.mpc]):
+    for key in keysToRemove:
+        if key in dict1:
+            del dict1[key]
+    return dict1
+
+
 if __name__ == "__main__":
     jsonPaths = ["dataset.json", Constants.pathToGeneralValues]
     mergeTwoJsonsAndShuffle(jsonPaths)
-
